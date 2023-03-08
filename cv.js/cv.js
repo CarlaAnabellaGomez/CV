c1810f2db3dfbb02db8df502da58e11625c136b1
Carla Gomez <gomez.charlie29@gmail.com>
2023-03-08 19:32:24 -0300

    Ultima modificacion 1

--- cv.js/cv.js
+++ cv.js/cv.js
@@ -1,7 +1,7 @@
 
 
 function  myFunction() {
-    ("div_perfil2")
+    ("button")
     document.getElementById("demo1").innerHTML = "Considero mi compromiso frente a la búsqueda de soluciones y alternativas a problemas.";
     document.getElementById("demo2").innerHTML = "Poseo optima adaptación a cambios.";
     document.getElementById("demo3").innerHTML = "También priorizo una relación empática con el entorno y el equipo de trabajo, desarrollándome proactivamente.";
